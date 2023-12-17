b8163f9282f7839487e270f591b3481aa5cc0196
Justin Langlinais <justinclanglinais@gmail.com>
2023-12-17 08:51:50 -0800

    fixing

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import Rank from './Components/Rank/Rank';
-import Particles from './Particles';
 
 
 function App() {
   return (
       <div>
-        <Particles />
         <Navigation />
         <Logo />
         <Rank />
